9eabf2d1d70112f75172df5ceb58aedf7d66e719
linkding <beninaoin@gmail.com>
2018-10-24 20:02:47 +0800

    add genisoimage

---
+++ genisoimage/install_genisoimage.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# -------------------------------------------------------------------------------
+# Script_name  : install_genisoimage.sh
+# Revision     : 1.0
+# Date         : 2018-10-24 17:12:29
+# Author       : Linkding
+# Email        : 619216759@qq.com
+# Description  : 安装 genisoimage 命令脚本
+# -------------------------------------------------------------------------------
+
+yum install genisoimage -y
